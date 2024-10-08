b80285df45305da41163d1247a4030eb289e4b15
Jeremy <jeremydnett@gmail.com>
2024-10-08 15:27:53 +1100

    nvim: Enable lsp_zero and set up LSP defaults

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,18 +1,49 @@
--- IMPORTANT: make sure to setup neodev BEFORE lspconfig
-require("neodev").setup({
-	-- add any options here, or leave empty to use the default settings
+-- Language servers
+local lsp_zero = require('lsp-zero')
+
+-- lsp_attach is where you enable features that only work
+-- if there is a language server active in the file
+local lsp_attach = function(client, bufnr)
+    local opts = {buffer = bufnr}
+
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+end
+
+lsp_zero.extend_lspconfig({
+    sign_text = true,
+    lsp_attach = lsp_attach,
+    capabilities = require('cmp_nvim_lsp').default_capabilities(),
 })
 
--- then setup your lsp server as usual
-local lspconfig = require('lspconfig')
+require('mason').setup({})
+require('mason-lspconfig').setup_handlers({
+    -- use handlers to avoid calling lsconfig or ensure_installed
+    function(server_name)
+        require('lspconfig')[server_name].setup({})
+    end,
+})
 
--- example to setup lua_ls and enable call snippets
-lspconfig.lua_ls.setup({
-	settings = {
-		Lua = {
-			completion = {
-				callSnippet = "Replace"
-			}
-		}
-	}
+-- Autocomplete
+local cmp = require('cmp')
+
+cmp.setup({
+    sources = {
+        {name = 'nvim_lsp'},
+    },
+    snippet = {
+        expand = function(args)
+            -- You need Neovim v0.10 to use vim.snippet
+            vim.snippet.expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({}),
 })
--- nvim/lua/plugin_specs.lua
+++ nvim/lua/plugin_specs.lua
@@ -29,16 +29,35 @@ local plugins = {
     {'tpope/vim-fugitive'},
     {'lervag/vimtex'},
 
-	-- LSP stuff
-	{'williamboman/mason.nvim'},
-	{'williamboman/mason-lspconfig.nvim'},
+    -- LSP stuff
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
 
-	--{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-	{'folke/neodev.nvim'},
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
-	{'L3MON4D3/LuaSnip'},
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp',
+        opts = function(_, opts)
+            opts.sources = opts.sources or {}
+            table.insert(opts.sources, {
+                name = "lazydev",
+                group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+            })
+        end,
+    },
+
+
+    {'folke/lazydev.nvim',
+        ft = "lua", -- only load on lua files
+        opts = {
+            library = {
+                -- See the configuration section for more details
+                -- Load luvit types when the `vim.uv` word is found
+                { path = "luvit-meta/library", words = { "vim%.uv" } },
+            },
+        },
+    },
+    {'Bilal2453/luvit-meta', lazy = true }, -- optional `vim.uv` typings
 }
 
 local opts = {}
